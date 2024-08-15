5b7dbfad63e16bf277e259a72f2b4c847aafe3ea
Oliveoil1 <47705687+Oliveoil1@users.noreply.github.com>
2024-08-15 13:37:38 +1200

    start implement settings window

---
+++ SubstandardApp/ViewModels/SettingsWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using SubstandardApp.Models;
+
+namespace SubstandardApp.ViewModels;
+
+public partial class SettingsWindowViewModel(SettingsModel settingsModel, ServerInfoModel serverInfoModel) : ViewModelBase
+{
+	[ObservableProperty] private SettingsModel _settingsModel = settingsModel;
+	[ObservableProperty] private ServerInfoModel _serverInfoModel = serverInfoModel;
+}
